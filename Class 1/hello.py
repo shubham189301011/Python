6e61b586cd90e170ad43a23d178f73ec0484641e
shubham189301011 <43439545+shubham189301011@users.noreply.github.com>
2018-09-26 23:41:28 +0530

    Update hello.py

--- Class 1/hello.py
+++ Class 1/hello.py
@@ -5,7 +5,7 @@ print('What is your name?') # ask for their name
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
-print(len(myName))
+print(len(myName)) #length of myName i.e. Shubham ki 7
 print('What is your age?') # ask for their age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
